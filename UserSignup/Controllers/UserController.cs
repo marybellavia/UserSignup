a7edb02fe448c67b90599c148d281285d716b158
Mary Bellavia <sauerwein.mary@gmail.com>
2020-01-02 19:16:02 -0600

    all workingggg

---
+++ UserSignup/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using UserSignup.Models;
+
+namespace UserSignup.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index(User user)
+        {
+            ViewBag.user = user;
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(User user, string verify)
+        {
+            ViewBag.user = user;
+            if (user.Username == null
+                || user.Email == null
+                || user.Password == null)
+            {
+                return View("Add");
+            }
+            else if (user.Password != verify)
+            {
+                ViewBag.verifyError = "Those passwords don't match!";
+                return View("Add");
+            }
+            return View("Index", user);
+        }
+    }
+}
---
+++ UserSignup/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserSignup.Models
+{
+    public class User
+    {
+        [Required(ErrorMessage = "You must input a username!")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "You must input an email address!")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "You must input a password!")]
+        public string Password { get; set; }
+
+        public User()
+        {
+
+        }
+    }
+}
